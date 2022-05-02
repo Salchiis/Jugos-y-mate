534fbfd990286168f22bf9b447f5ec58644a88b9
Arturo Montes de Oca Barrios <48523592+Weebmaster666@users.noreply.github.com>
2022-05-02 13:34:22 -0500

    Loaded project

---
+++ IOSProject/IOSProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOSProject
+//
+//  Created by Alumno on 29/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+    
+    
